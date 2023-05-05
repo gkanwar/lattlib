c07d1c3f85e773b2008bcac8fe11fc81cee84b07
Gurtej Kanwar <gurtej@mit.edu>
2023-05-05 14:06:21 +0200

    Tweaks paper_plt loading to easily enable plt.rc_context

--- pylib/paper_plt.py
+++ pylib/paper_plt.py
@@ -3,33 +3,45 @@
 import matplotlib.pyplot as plt
 
 pix_to_pt = 72/100
+
+latex_config = {
+    'text.usetex': True,
+    'text.latex.preamble': r'\usepackage{amsmath,amssymb}',
+    'font.serif': ['Computer Modern Roman']
+}
+
 def load_latex_config():
-    plt.rcParams['errorbar.capsize'] = 1
-    plt.rcParams['lines.linewidth'] = 1.0 * pix_to_pt
-    plt.rcParams['lines.markeredgewidth'] = 1.0 * pix_to_pt
-    plt.rcParams['savefig.bbox'] = 'tight'
-    plt.rcParams['axes.labelsize'] = 9
-    plt.rcParams['axes.linewidth'] = 0.5 * pix_to_pt
-    plt.rcParams['lines.markersize'] = 2
-    plt.rcParams['xtick.labelsize'] = 8
-    plt.rcParams['ytick.labelsize'] = 8
-    plt.rcParams['xtick.major.width'] = 0.5 * pix_to_pt
-    plt.rcParams['xtick.minor.width'] = 0.5 * pix_to_pt
-    plt.rcParams['ytick.major.width'] = 0.5 * pix_to_pt
-    plt.rcParams['ytick.minor.width'] = 0.5 * pix_to_pt
-    plt.rcParams['legend.fontsize'] = 8
-    plt.rcParams['text.usetex'] = True
-    plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath,amssymb}'
-    plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.serif'] = ['Computer Modern Roman']
-    plt.rcParams['legend.fancybox'] = False
-    plt.rcParams['legend.shadow'] = False
-    plt.rcParams['legend.framealpha'] = '0.8'
-    plt.rcParams['legend.facecolor'] = 'white'
-    plt.rcParams['legend.edgecolor'] = '0.5'
-    plt.rcParams['patch.linewidth'] = 0.5 * pix_to_pt
-    plt.rcParams['legend.handlelength'] = 1.0
-    plt.rcParams['legend.fontsize'] = 8
+    load_basic_config()
+    plt.rcParams.update(latex_config)
+
+basic_config = {
+    'lines.linewidth': 1.0 * pix_to_pt,
+    'lines.markeredgewidth': 1.0 * pix_to_pt,
+    'savefig.bbox': 'tight',
+    'axes.labelsize': 9,
+    'axes.linewidth': 0.5 * pix_to_pt,
+    'lines.markersize': 2,
+    'errorbar.capsize': 1,
+    'xtick.labelsize': 8,
+    'ytick.labelsize': 8,
+    'xtick.major.width': 0.5 * pix_to_pt,
+    'xtick.minor.width': 0.5 * pix_to_pt,
+    'ytick.major.width': 0.5 * pix_to_pt,
+    'ytick.minor.width': 0.5 * pix_to_pt,
+    'legend.fontsize': 8,
+    'legend.fancybox': False,
+    'legend.shadow': False,
+    'legend.framealpha': '0.8',
+    'legend.facecolor': 'white',
+    'legend.edgecolor': '0.5',
+    'patch.linewidth': 0.5 * pix_to_pt,
+    'legend.handlelength': 1.0,
+    'legend.fontsize': 8,
+    'font.family': 'serif'
+}
+
+def load_basic_config():
+    plt.rcParams.update(basic_config)
 
 def inner_ylabel(ax, label, *, x, y, labelpad, **kwargs):
     ax.set_ylabel(label, x=x, y=y, labelpad=labelpad, rotation='horizontal', fontsize=10, **kwargs)
