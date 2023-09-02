5a4e664b857fc4ed0b7b23f669b14d02fec5ef44
Gurtej Kanwar <gurtej@mit.edu>
2023-09-02 17:20:13 +0200

    Adds det normalization to SU(N) Haar sampling

--- suN_haar/suN_haar.py
+++ suN_haar/suN_haar.py
@@ -2,10 +2,17 @@
 
 import numpy as np
 
-def generate_suN_haar(Nc, *, size):
+def generate_uN_haar(Nc, *, size):
     shape = (size, Nc, Nc)
     Z = np.random.normal(size=shape) + 1j * np.random.normal(size=shape)
     Q, R = np.linalg.qr(Z)
     Lam = np.exp(1j * np.angle(np.einsum('...ii->...i', R)))
     U = np.einsum('...ab,...b -> ...ab', Q, Lam)
     return U
+
+def generate_suN_haar(Nc, *, size):
+    U = generate_uN_haar(Nc, size=size)
+    theta = np.angle(np.linalg.det(U))
+    z = 2*np.pi*np.random.randint(Nc, size=size)
+    Up = U * np.exp(1j * (z - theta) / Nc)[...,np.newaxis,np.newaxis]
+    return Up
